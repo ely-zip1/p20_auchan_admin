1dc8aa7a86f877cf1757e73f2759aeaf612b962d
ely-zip1 <elisha.lapiz@gmail.com>
2023-07-15 03:26:53 +0800

    save

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
- $config['base_url'] = 'http://localhost/p20_auchan_admin/';
-// $config['base_url'] = 'https://admin.investorauchan.com/';
+//  $config['base_url'] = 'http://localhost/p20_auchan_admin/';
+$config['base_url'] = 'https://admin.investorauchan.com/';
 
 
 
